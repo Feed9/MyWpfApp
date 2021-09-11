519362a6838c5fb5162e0515357dd97d5f6ce024
haxzoor <haxzoor1337@mail.ru>
2021-09-12 00:03:24 +0300

    Added CommandBase class

---
+++ MyWpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MyWpfApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpfApp.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _title = "Default Name";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+
+        }
+
+        private int _progress = 90;
+        /// <summary>
+        /// Прогресс
+        /// </summary>
+        public int Progress
+        {
+            get => _progress;
+            set => Set(ref _progress, value);
+
+        }
+
+
+
+    }
+}
