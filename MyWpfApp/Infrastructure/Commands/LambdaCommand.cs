7033bd3748bfc51a697ab26dcd87fddc03d4a3fe
haxzoor <haxzoor1337@mail.ru>
2021-09-12 00:20:49 +0300

    Released a simple command

---
+++ MyWpfApp/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using MyWpfApp.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpfApp.Infrastructure.Commands
+{
+    class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _canExecute = canExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+           return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter) => _execute(parameter);        
+    }
+}
